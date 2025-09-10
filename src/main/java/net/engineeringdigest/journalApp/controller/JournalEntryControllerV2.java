832ec74da9aaafb5d2859a56ba13b66d3cd41d1f
musab702 <musabshaikh702@gmail.com>
2025-09-10 13:14:59 -0400

    added journal entry service with Mongo

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -0,0 +1,47 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.service.JournalEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return null;
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntryService.saveEntry(myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntriesById(@PathVariable Long myId){
+        return null;
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteEntryById(@PathVariable Long myId){
+        return null;
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry modifyEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        return null;
+    }
+
+}
