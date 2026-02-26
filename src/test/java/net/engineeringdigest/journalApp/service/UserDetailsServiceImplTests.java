54df6937a980fa894184c1bba78c63061b25471b
musab702 <musabshaikh702@gmail.com>
2026-02-25 20:21:06 -0500

    peep

--- src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
+@ActiveProfiles("dev")
 public class UserDetailsServiceImplTests {
 
     @InjectMocks
